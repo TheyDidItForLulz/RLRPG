d5cdbaa3dc7253236614f21439aa2b936d84107c
Yuri12358 <hydra12358@gmail.com>
2017-02-08 15:41:15 +0200

    fixed drinking all of potions by one command

--- main.cpp
+++ main.cpp
@@ -1410,7 +1410,14 @@ public:
 							message += "Now you feeling better. ";
 							break;
 					}
-					inventory[intch].type = ItemEmpty;
+					if( inventory[intch].GetItem().count == 1 )
+					{
+						inventory[intch].type = ItemEmpty;
+					}
+					else
+					{
+						--inventory[intch].GetItem().count;
+					}
 				}
 				break;
 			}
